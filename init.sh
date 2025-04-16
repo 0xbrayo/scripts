395fcadc830f6b816f4af2a0d036d835e8746570
Brayo <vukubrian@gmail.com>
2025-04-16 17:17:39 +0300

    feat: update arch scripts

--- init.sh
+++ init.sh
@@ -1,3 +1,21 @@
-sudo pacman -Syu --noconfirm gnome-tweaks
-sudo pacman -Syu --noconfirm ffmpeg
-sudo pacman -Syu --noconfirm python-pip
+sudo pacman -R --noconfirm gnome-text-editor \
+    gnome-calculator \
+    yelp \ 
+    yelp-xsl
+sudo pacman -Syu
+sudo pacman -S --noconfirm gnome-tweaks \
+    ffmpeg \
+    python-pip \vlc \
+    ripgrep \
+    gh
+    
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
+gsettings set org.gnome.desktop.interface clock-show-date "false"
+
+git config --global user.name Brayo
+git config --global user.email vukubrian@gmail.com
+
+timedatectl set-timezone Africa/Nairobi
+    
+echo 🧡✘🤖
